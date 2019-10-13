550f0bf04bc0ced31adb18091b8e4673663c1616
Breno Viana <brenomfviana@gmail.com>
2019-10-13 20:16:20 -0300

    finishes Crates chapter

---
+++ 11-Crates/1-extern-crate.rs
@@ -0,0 +1,11 @@
+// To compile use rustc executable.rs --extern rary=library.rlib
+
+// Link to `library`, import items under the `rary` module
+extern crate rary;
+
+fn main() {
+  rary::public_function();
+  // Error! `private_function` is private
+  //rary::private_function();
+  rary::indirect_access();
+}
